a802bb30bea513da4e73518281eef9fbe43539f8
nunoluciano <1905497+gigamaster@users.noreply.github.com>
2025-11-03 09:47:07 +0100

    utils

--- src/static-build/utils.tsx
+++ src/static-build/utils.tsx
@@ -62,12 +62,11 @@ export function escapeStyleScriptContent(str: string): string {
  */
 export const siteOrigin = (() => {
   if (process.env.DEV_PORT) return `http://localhost:${process.env.DEV_PORT}`;
-  // https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
   if (process.env.CONTEXT === 'production')
-    return 'https://gigamaster.github.io';
+    return 'https://gigamaster.github.io/codemo-image-optimize';
   if (process.env.DEPLOY_PRIME_URL) return process.env.DEPLOY_PRIME_URL;
   console.warn(
-    'Unable to determine site origin, defaulting to https://gigamaster.github.io',
+    'Unable to determine site origin, defaulting to https://gigamaster.github.io/codemo-image-optimize',
   );
-  return 'https://gigamaster.github.io';
+  return 'https://gigamaster.github.io/codemo-image-optimize';
 })();
